8dcd9e8da418434da20a280a5a175b1f746b7f15
nighca <nighca@live.cn>
2021-05-18 11:50:38 +0800

    debug sample common-chunk

--- common-chunk/src/entry-2.js
+++ common-chunk/src/entry-2.js
@@ -1,10 +1,10 @@
-import { log } from './common-1'
-import { warn } from './common-2'
+import { log } from 'common-1'
+import { warn } from 'common-2'
 import './style.css'
 
 log(2)
 warn(2)
-import('./common-async-1').then(
+import('common-async-1').then(
   ({ error }) => error(2)
 )
 import('./common-async-2').then(
--- common-chunk/src/vendor.js
+++
@@ -1,3 +0,0 @@
-import './common-1'
-import './common-2'
-import './common-async-1'
